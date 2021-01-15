edbf73700227d9e4bcfb15cda9dab0baf5bc0c28
thiago morgado <thiagomorgado81@gmail.com>
2021-01-15 15:28:03 -0300

    base dedados pronta

--- api/src/database/migrations/00_create_points.ts
+++ api/src/database/migrations/00_create_points.ts
@@ -1,6 +1,18 @@
-export async function up() {
-  
+import Knex from 'knex'
+
+export async function up(knex: Knex) {
+  return await knex.schema.createTable('points',  table => {
+     table.increments('id').primary();
+     table.string('image').notNullable();
+     table.string('name').notNullable();
+     table.string('email').notNullable();
+     table.string('whatsapp').notNullable();
+     table.decimal('latitude').notNullable();
+     table.decimal('longetude').notNullable();
+     table.string('city').notNullable();
+     table.string('uf', 2).notNullable(); 
+  });
 }
-export async function doen() {
-  
+export async function down(knex: Knex) {
+  return await knex.schema.dropTable('points')
 }
