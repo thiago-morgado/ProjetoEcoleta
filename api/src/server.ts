2521bcf9cc6e92bf1481518fd5c498d845159ee2
thiago morgado <thiagomorgado81@gmail.com>
2021-01-15 18:21:17 -0300

    criando rotas de imagem

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import Routes from './routes';
-import routes from './routes';
+import path from 'path'
 
 const App = express();
 
 App.use(express.json())
 App.use(Routes);
+App.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 
 
